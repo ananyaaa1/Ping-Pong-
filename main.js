7053f21589afa7c14b41af178f38800fad897f5b
ananyaaa1 <96527004+ananyaaa1@users.noreply.github.com>
2022-06-17 15:48:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -24,13 +24,22 @@ var ball = {
 function setup(){
   var canvas =  createCanvas(700,600);
   canvas.parent("canvas");
+  video = createCapture(VIDEO);
+  video.size(700,600);
+  video.hide();
+  poseNet = ml5.poseNet (video,modelLoaded);
+}
+
+function modelLoaded()
+{
+  console.log("Model Loaded"); 
 }
 
 
 function draw(){
 
  background(0); 
-
+ image(video,0,0,700,600);
  fill("black");
  stroke("black");
  rect(680,0,20,700);
